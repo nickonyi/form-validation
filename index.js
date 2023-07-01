c4900a8de05fbe3982bc004fa81391f5ccc9601a
nickonyi <nickoluoch1130@gmail.com>
2023-07-01 12:03:55 +0300

    form validation

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 const form = document.getElementById('form');
-const email = document.getElementById('email')
+const formFields = document.querySelectorAll('.form-fields');
+const email = document.getElementById('email');
 const zipCode = document.getElementById('zip-code');
 const country = document.getElementById('country')
 const password = document.getElementById('password');
-const cPassword = document.getElementById('password2');
-activateInputs();
-validatingForm();
+const password2 = document.getElementById('password2');
 
-
-
-
-
-
-
-
-function validatingForm() {
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-
-        validateInputs();
+formFields.forEach(field => {
+    field.addEventListener('blur', (e) => {
+        validateForm(field);
     });
-}
+
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    formFields.forEach(field => {
+        validateForm(field);
+    });
+
+
+    const errors = document.querySelectorAll('.error');
+    const successBox = document.querySelector('.success-payment-popup');
+
+    if (errors.length === 5) {
+        form.classList.add('hidden');
+        successBox.classList.remove('hidden');
+    }
+});
+
 
 function setErrorMessage(element, message) {
     const inputControl = element.parentElement;
@@ -47,71 +55,59 @@ const isValidEmail = email => {
     return re.test(String(email).toLowerCase());
 }
 
-function validateInputs() {
 
-    const emailValue = email.value.trim();
-    const zipCodeValue = zipCode.value.trim();
-    const countryValue = country.value.trim();
-    const passwordValue = password.value.trim();
-    const cPasswordValue = cPassword.value.trim();
 
-    if (emailValue == "") {
-        setErrorMessage(email, "The email is required");
-    } else if (!isValidEmail(emailValue)) {
-        setErrorMessage(email, "Put a valid email");
-    } else {
-        setSuccess(email);
+
+
+function validateForm(field) {
+    const value = field.value.trim();
+    // Perform validation based on field ID
+    if (field.id == "email") {
+        if (value == "") {
+            setErrorMessage(email, "We need an email wewe mzee");
+        } else if (!isValidEmail(value)) {
+            setErrorMessage(email, 'Wewe mzee eka email mzuri!');
+        } else {
+            setSuccess(email);
+        }
     }
 
-    if (zipCodeValue == "") {
-        setErrorMessage(zipCode, "The zip code is required");
-    } else {
-        setSuccess(zipCode);
+    if (field.id == 'zip-code') {
+        if (value == "") {
+            setErrorMessage(zipCode, "Wewe mzee tunataka kujua pali unaishi!");
+
+        } else {
+            setSuccess(zipCode);
+        }
     }
 
-    if (countryValue == "") {
-        setErrorMessage(country, "The country value is required");
-    } else {
-        setSuccess(country);
+
+    if (field.id == 'country') {
+        if (value == "") {
+            setErrorMessage(country, "Hata hiyo nchi tunataka kujua pia");
+        } else {
+            setSuccess(country);
+        }
     }
 
-    if (passwordValue == "") {
-        setErrorMessage(password, "Password is required");
-    } else if (passwordValue.length < 8) {
-        setErrorMessage(password, "The password is less than 8 characters");
-    } else {
-        setSuccess(password);
+    if (field.id == 'password') {
+        if (value == "") {
+            setErrorMessage(password, "Paaaaasuad my guy!");
+        } else if (value.length < 8) {
+            setErrorMessage(password, "Na passuuad ikue strong mzee!!");
+        } else {
+            setSuccess(password);
+        }
     }
 
-    if (cPasswordValue == "") {
-        setErrorMessage(cPassword, "Please confirm password");
-    } else if (passwordValue !== cPasswordValue) {
-        setErrorMessage(cPassword, "The password does not match!");
-    } else {
-        setSuccess(cPassword);
+    if (field.id == 'password2') {
+        const passwordValue = password.value.trim();
+        if (value == "") {
+            setErrorMessage(password2, "Hebu rudia hio paaasuuuad tena!");
+        } else if (passwordValue != value) {
+            setErrorMessage(password2, "We mzee pasuaaad hazifanani!");
+        } else {
+            setSuccess(password2);
+        }
     }
-}
-
-
-
-
-function activateInputs() {
-    // Select all input elements
-    var inputs = document.querySelectorAll("input");
-
-    // Add event listener for focusin
-    inputs.forEach(function(input) {
-        input.addEventListener("focusin", function() {
-            this.parentNode.querySelector("label").classList.add("active");
-        });
-    });
-
-    // Add event listener for focusout
-    inputs.forEach(function(input) {
-        input.addEventListener("focusout", function() {
-            if (!this.value) {
-                this.parentNode.querySelector("label").classList.remove("active");
-            }
-        });
-    });
 }
