e9abd68ad854b35773609bed01df7c6a09289d99
nickonyi <nickoluoch1130@gmail.com>
2026-07-06 17:32:54 +0300

    feat: set up users router and integrate EJS as view engine

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 import express from "express";
+import url from "url";
+import path from "path";
+import usersRouter from "./routes/usersRouter.js";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use("/", usersRouter);
 
 const app = express();
 
